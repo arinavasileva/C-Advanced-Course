6730829b2086179e86315757b55ca85544df9989
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2024-09-10 12:23:47 +0300

    Update lab02.cpp

--- Project2/Project2/lab02.cpp
+++ Project2/Project2/lab02.cpp
@@ -12,7 +12,7 @@ int main() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(1, 37);
 
-    // Generate two sets of 7 unique lotto numbers
+    // Generating two sets of 7 unique lotto numbers
     std::vector<int> lotto1, lotto2;
     while (lotto1.size() < 7) {
         int num = dist(gen);
@@ -23,20 +23,20 @@ int main() {
         if (std::find(lotto2.begin(), lotto2.end(), num) == lotto2.end()) lotto2.push_back(num);
     }
 
-    // Print both sets using output stream iterator
+    // Printing both sets using output stream iterator
     std::cout << "First lotto row: ";
     std::copy(lotto1.begin(), lotto1.end(), std::ostream_iterator<int>(std::cout, " "));
     std::cout << "\nSecond lotto row: ";
     std::copy(lotto2.begin(), lotto2.end(), std::ostream_iterator<int>(std::cout, " "));
     std::cout << std::endl;
 
-    // Find common numbers
+    // Finding common numbers
     std::sort(lotto1.begin(), lotto1.end());
     std::sort(lotto2.begin(), lotto2.end());
     std::vector<int> common_numbers;
     std::set_intersection(lotto1.begin(), lotto1.end(), lotto2.begin(), lotto2.end(), std::back_inserter(common_numbers));
 
-    // Print common numbers using for_each
+    // Printing common numbers using for_each
     std::cout << "Same numbers:\n";
     int idx = 1;
     std::for_each(common_numbers.begin(), common_numbers.end(), [&idx](int num) {
